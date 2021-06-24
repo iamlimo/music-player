a85feca9c6e77e9552327c95cd686da04b03b272
Isaac orija <isaac.orija@gmail.com>
2021-06-24 01:07:23 +0100

    trying out react

---
+++ src/components/Recommended.js
@@ -0,0 +1,43 @@
+import axios from 'axios';
+import React, {useState, useEffect} from 'react'
+import ImageCover from '../assets/b-e.jpg'
+import '../styles/recommended.css'
+
+const Recommended = () => {
+    const [songs, displaySongs] = useState([]);
+
+
+    useState(()=>{
+        const fetchSongs = async () =>{
+            const res = await axios.get('https://api.napster.com/v2.1/tracks/top?apikey=ZTk2YjY4MjMtMDAzYy00MTg4LWE2MjYtZDIzNjJmMmM0YTdm');
+
+        let newData = res.data.tracks
+            
+        displaySongs(newData)
+            
+        }      
+        fetchSongs()
+       
+    }, [])
+
+    console.log(songs)
+    return (
+        <>
+         
+         {songs.map((song) =>{
+             
+             return   <div className="recommended">
+            <div id="img-cover" className="" style={{ backgroundImage: `url(http://direct.rhapsody.com/imageserver/v2/albums/${song.albumId}/images/300x300.jpg)` }}></div>
+            <h3 id="">{song.artistName}</h3>
+            <p>I love you babe</p>
+            </div>
+         })}
+           
+            
+
+         
+        </>
+    )
+}
+
+export default Recommended
---
+++ src/pages/LandingPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Recommended from '../components/Recommended'
+const LandingPage = () => {
+    return (
+        <div>
+         <Recommended></Recommended>
+        </div>
+    )
+}
+
+export default LandingPage
